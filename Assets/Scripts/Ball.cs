e2adca506e73bb595715dab46444e1c960c99212
Unknown <majuris1@hawkmail.newpaltz.edu>
2017-03-02 11:04:40 -0500

    Added Unity Files

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Vector2 startingVelocity = new Vector2(15, -20);
+
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = startingVelocity;
+	}
+
+
+	void Update () {
+		if(transform.position.y < -2.4f) {
+			GetOut();
+		}
+	}
+
+	void GetOut()
+	{
+		Debug.Log("You are out");
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+	public float speed = 5;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw("Horizontal");
+		x = x * speed;
+		rigidBody.velocity = new Vector2(x, 0);
+	}
+}
